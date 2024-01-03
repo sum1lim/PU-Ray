6cfcdede25337bc0ee3d1fe7f2ab83e3a08d4e86
Sangwon Lim <sangwon3@ualberta.ca>
2024-01-02 19:33:46 -0700

    kitti

--- upsample_real.sh
+++ upsample_real.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+for FILE in ./data/kitti/*; do
+    pu_ray --input "$FILE" --r 4  --patch-k 16 --query-k 32 --marching-steps 6 --model puray_supervised --output-dir kitti --real-scanned-snippet --fps
+done
+
 for FILE in ./data/HDL_dataset_te/resolution_0.200_occ/*; do
     pu_ray --input "$FILE" --r 4  --patch-k 16 --query-k 32 --marching-steps 6 --model puray_supervised --output-dir resolution_0.20_occ --real-scanned --fps
 done
-
