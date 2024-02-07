b3525b85d90c8d547144544980bea3f4077ec50f
Sangwon Lim <sangwon3@ualberta.ca>
2024-02-06 17:18:59 -0700

    min dist of 30000

--- upsample_real.sh
+++ upsample_real.sh
@@ -28,7 +28,7 @@ for FILE in ./data/kitti_test/resolution_h0.200_v0.400_occ/*; do
     evaluate --pc1 ./data/kitti_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/kitti_complext2test/"$(basename ${FILE%.*}).xyz" --log ./log/kitti_complext2test.log --wait  --min-dist 15000
 done
 
-generate_gt --aoi ./data/highway_test/resolution_h0.200_v0.400_occ --reference ./data/highway_test/reference --min-dist 25000
+generate_gt --aoi ./data/highway_test/resolution_h0.200_v0.400_occ --reference ./data/highway_test/reference --min-dist 30000
 rm ./log/highway_puray.log
 rm ./log/highway_train2test.log
 rm ./log/highway_test2test.log
@@ -38,16 +38,16 @@ rm ./log/highway_complext2test.log
 
 for FILE in ./data/highway_test/resolution_h0.200_v0.400_occ/*; do
     echo "******************** $(basename ${FILE%.*}) ********************"
-    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_supervised --output-dir highway_puray --real-scanned --min-dist 25000 --fps
-    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_puray/"$(basename ${FILE%.*}).xyz" --log ./log/highway_puray.log --wait --min-dist 25000
-    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_train2test --output-dir highway_train2test --real-scanned --min-dist 25000 --fps
-    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_train2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_train2test.log --wait --min-dist 25000
-    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_test2test --output-dir highway_test2test --real-scanned --min-dist 25000 --fps
-    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_test2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_test2test.log --wait --min-dist 25000
-    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_simple2test --output-dir highway_simple2test --real-scanned --min-dist 25000 --fps
-    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_simple2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_simple2test.log --wait --min-dist 25000
-    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_medium2test --output-dir highway_medium2test --real-scanned --min-dist 25000 --fps
-    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_medium2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_medium2test.log --wait --min-dist 25000
-    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_complex2test --output-dir highway_complext2test --real-scanned --min-dist 25000 --fps
-    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_complext2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_complext2test.log --wait --min-dist 25000
+    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_supervised --output-dir highway_puray --real-scanned --min-dist 30000 --fps
+    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_puray/"$(basename ${FILE%.*}).xyz" --log ./log/highway_puray.log --wait --min-dist 30000
+    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_train2test --output-dir highway_train2test --real-scanned --min-dist 30000 --fps
+    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_train2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_train2test.log --wait --min-dist 30000
+    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_test2test --output-dir highway_test2test --real-scanned --min-dist 30000 --fps
+    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_test2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_test2test.log --wait --min-dist 30000
+    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_simple2test --output-dir highway_simple2test --real-scanned --min-dist 30000 --fps
+    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_simple2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_simple2test.log --wait --min-dist 30000
+    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_medium2test --output-dir highway_medium2test --real-scanned --min-dist 30000 --fps
+    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_medium2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_medium2test.log --wait --min-dist 30000
+    pu_ray --input "$FILE" --r 4  --batch-size 2500 --patch-k 64 --query-k 8 --marching-steps 6 --model puray_complex2test --output-dir highway_complext2test --real-scanned --min-dist 30000 --fps
+    evaluate --pc1 ./data/highway_test/resolution_h0.200_v0.400_occ_gt/"$(basename ${FILE%.*}).csv" --pc2 ./output/highway_complext2test/"$(basename ${FILE%.*}).xyz" --log ./log/highway_complext2test.log --wait --min-dist 30000
 done
